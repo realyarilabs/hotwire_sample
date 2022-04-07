10b7effb41536805fe0e61cc1bd25f80777e5e2d
Emanuel Mota <emanuel@yarilabs.com>
2022-04-07 17:01:20 +0100

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def turbo_frame_form
+  end
+
+  def turbo_frame_submit
+    extracted_anynumber = params[:any][:anynumber]
+    render :turbo_frame_form, status: :ok, locals: {
+      anynumber: extracted_anynumber, comment: 'turbo_frame_submit ok'
+    }
+  end
+end
